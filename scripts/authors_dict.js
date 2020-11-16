ea6097350381a82d97d29ba6fc2c838fa8175712
Nikita <huskyray@mail.ru>
2020-11-17 01:18:50 +0300

    authors Map

---
+++ scripts/authors_dict.js
@@ -0,0 +1,9 @@
+var authors = new Map();
+authors.set('И. Ф. Анненский', ['annenskiy_i.jpg', 'Иннокентий Анненский']);
+authors.set('К. Д. Бальмонт', ['balmont_k.png', 'Константин Бальмонт']);
+authors.set('А. Белый', ['beliy_a.png', 'Андрей Белый']);
+authors.set('А. Блок', ['blok_a.jpg', 'Александр Блок']);
+authors.set('В. Я. Брюсов', ['brusov_v.png', 'Валерий Брюсов']);
+
+
+
